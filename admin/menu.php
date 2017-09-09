a1e25e4f6e1fd04d9717eca7cd83ca8a9195c620
mambax7 <mambax7@gmail.com>
2017-09-09 09:59:28 -0400

    menu standardization

--- admin/menu.php
+++ admin/menu.php
@@ -28,45 +28,45 @@ $moduleHelper->loadLanguage('modinfo');
 // Index
 $adminmenu              = [];
 $i                      = 0;
-$adminmenu[$i]['title'] = _AM_MODULEADMIN_HOME;
-$adminmenu[$i]['link']  = 'admin/index.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/home.png';
+'title' =>  _AM_MODULEADMIN_HOME,
+'link' =>  'admin/index.php',
+'icon' =>  $pathIcon32 . '/home.png',
 
 // User
 ++$i;
-$adminmenu[$i]['title'] = MPU_MOD_MENU_ADD;
-$adminmenu[$i]['link']  = 'admin/main.php?op=novo';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/add.png';
+'title' =>  MPU_MOD_MENU_ADD,
+'link' => novo',
+'icon' =>  $pathIcon32 . '/add.png',
 
 // List Content
 ++$i;
-$adminmenu[$i]['title'] = MPU_MOD_MENU_LST;
-$adminmenu[$i]['link']  = 'admin/main.php?op=listar';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/manage.png';
+'title' =>  MPU_MOD_MENU_LST,
+'link' => listar',
+'icon' =>  $pathIcon32 . '/manage.png',
 
 // HTML Files
 ++$i;
-$adminmenu[$i]['title'] = MPU_MOD_MENU_LNK;
-$adminmenu[$i]['link']  = 'admin/paginas.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/content.png';
+'title' =>  MPU_MOD_MENU_LNK,
+'link' =>  'admin/paginas.php',
+'icon' =>  $pathIcon32 . '/content.png',
 
 // Media
 ++$i;
-$adminmenu[$i]['title'] = MPU_MOD_MENU_MED;
-$adminmenu[$i]['link']  = 'admin/media.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/marquee.png';
+'title' =>  MPU_MOD_MENU_MED,
+'link' =>  'admin/media.php',
+'icon' =>  $pathIcon32 . '/marquee.png',
 
 ++$i;
-$adminmenu[$i]['title'] = MPU_MOD_MENU_FIL;
-$adminmenu[$i]['link']  = 'admin/files.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/folder_txt.png';
+'title' =>  MPU_MOD_MENU_FIL,
+'link' =>  'admin/files.php',
+'icon' =>  $pathIcon32 . '/folder_txt.png',
 
 //++$i;
-//$adminmenu[$i]['title'] = _MI_SYSTEM_ADMENU2;
-//$adminmenu[$i]['link'] =  'admin/blocksadmin.php';
+//'title' =>  _MI_SYSTEM_ADMENU2,
+//'link' =>   'admin/blocksadmin.php',
 //$adminmenu[$i]["icon"] = $pathIcon32.'/export.png';
 
 ++$i;
-$adminmenu[$i]['title'] = _AM_MODULEADMIN_ABOUT;
-$adminmenu[$i]['link']  = 'admin/about.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/about.png';
+'title' =>  _AM_MODULEADMIN_ABOUT,
+'link' =>  'admin/about.php',
+'icon' =>  $pathIcon32 . '/about.png',
